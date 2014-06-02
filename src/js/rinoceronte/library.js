ffea372564403ba71c54edaaa387cecceb171a2e
gomezd <david.garcia.mx@gmail.com>
2014-06-01 23:21:56 -0700

    Fixed indentation.

--- src/js/rinoceronte/library.js
+++ src/js/rinoceronte/library.js
@@ -1,12 +1,11 @@
 
-var lib = Java.type('rinoceronte.Library'),
+var global = this,
+    lib = Java.type('rinoceronte.Library');
 
-    global = this;
+global.sayMyName = lib.sayMyName,
 
-    global.sayMyName = lib.sayMyName,
-
-    global.threeTimes = function (name) {
-        for (var i = 1; i <= 3; i++) {
-            sayMyName(i + ': ' +name);
-        }
-    };
+global.threeTimes = function (name) {
+    for (var i = 1; i <= 3; i++) {
+        sayMyName(i + ': ' +name);
+    }
+};
